42051021f050cfc081a14db19f0196059905d19f
grir <irus.grinis@gmail.com>
2024-02-03 19:23:37 +0200

    Update pvz01.cpp

--- pvz01/pvz01.cpp
+++ pvz01/pvz01.cpp
@@ -10,7 +10,7 @@ int suma(const int a, const int b, const int c){
 // Dar viena paprasta funkcija. Turime numatytuousius parametrus.
 int didziausioIrMaziausioSandauga(const int a=0, const int b=0, const int c=0){
       int mx = a;
-      int mn = a;
+      int mn = a; 
       if (b > mx) mx = b;
       if (b < mn) mn = b;
       if (c > mx) mx = c;
